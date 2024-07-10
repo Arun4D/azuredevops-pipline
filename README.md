6bac0c4c33ab35c2033548301742639f0d8f216e
arun <arun4.duraisamy@gmail.com>
2024-07-11 00:33:00 +0530

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # azuredevops-pipline
+
+## Scripts
+
+### [Create Repo](./createRepo.sh)
+
+create repo if repo not exists.
+
+* Note: replace <<PAT base64 encode|>> before run the script
+
+````
+createRepo.sh microservice-repo1
+````
---
+++ createRepo.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+#GET https://dev.azure.com/{organization}/{project}/_apis/git/repositories?api-version=7.1-preview.1
+
+#echo -n 'Arun Duraisamy:<<PAT>>' | base64
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <microservice name>"
+  exit 1
+fi
+
+checkName=$1
+#checkName='microservice-repo1'
+
+organization='arun4duraisamy0719'
+projectName='Bicep-demo'
+projectId='ebf97fe7-b4eb-4ad2-8d6c-2103d3c9f521'
+
+isNameExists=0
+
+echo "=============scrit start ========================="
+
+GetRepoResponse=$(curl --location --request GET "https://dev.azure.com/$organization/$projectName/_apis/git/repositories?api-version=7.1-preview.1" -H 'Authorization: Basic <<PAT encode>>' -H 'Cache-Control: no-cache')
+
+echo $GetRepoResponse
+echo ""
+
+for row in $(echo "$GetRepoResponse" | jq -c '.value[]'); do
+  name=$(echo "$row" | jq -r '.name')
+  if [ "$name" = "$checkName" ]; then
+    echo "Repo Name $name found, exiting loop."
+    echo ""
+    isNameExists=1
+    break
+  fi
+done
+
+if [ "$isNameExists" -eq 0 ]; then
+    echo "Repo Name $name not found"
+    echo ""
+    echo "Creating repo $name "
+    echo ""
+    CreateRepoResponse=$(curl --location --request POST "https://dev.azure.com/$organization/$project/_apis/git/repositories?api-version=7.1-preview.1" -H 'Authorization: Basic <<PAT encode>>' -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' --data '{"name":"'${checkName}'","project": {  "id": "'${projectId}'" } }')
+    echo $CreateRepoResponse
+    echo ""
+    repoUrl=$(echo "$CreateRepoResponse" | jq -r '.url') 
+    echo "Repo url: $repoUrl"
+    echo ""
+fi
+
+
+echo "=============scrit end  ============================"
